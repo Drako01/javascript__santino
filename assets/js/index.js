f609bb41ab76d1725ef0e7246da405eb2a38a15c
Alejandro Di Stefano <addistefano@live.com.ar>
2024-07-30 21:14:46 -0300

    Clase 01

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+// Declaracion de Variables
+// Numbers  > Numeros: Enteros o Decimales
+// Strings > Combinacion de Letras, letras y numeros, letras y caracteres especiales, o todo mezclado
+
+// camelCase
+// var numeroEntero = 1;
+// var numeroDecimal = 1.56;
+// var letra = 'a';
+// var string = "Santino";
+
+// var numeroEntero = "Alejandro";
+
+// Solucion > Usando let y const
+// Declaracion 
+let variable;
+// Para inicializarla se le asigna un valor
+variable = 12;
+
+let nuevaVariable = "Alejandro";
+// Reasignar a la Variable
+nuevaVariable = "Santino"; // String > Combinacion de caracteres 
+nuevaVariable = "@_4545asdasdaASFDFSD_-+da";
+nuevaVariable = 12; // Enteros o Integer
+nuevaVariable = 16.98; // Flotante o Decimales > float
+
+// Variables Primitivas
+let letra = 'a'; // Caracter
+let numero = 1; // Number > Integer
+
+// Formas de como NO declarar Variables
+
+// let 1letra = 0;
+// let @hola = 2;
+
+
+let _hola = 1; // Unico caracter especial para declarar variables que esta permitido
+
+// Constantes
+const constante = 1;
+// constante = 2; > NO se puede reasignar
+// console.log("Hola mundo") > Funcion de JavaScript que me permite enviar un mensaje a la consola
+
+console.log("Mensaje enviado a la consola");
+console.log(constante);
+console.log(_hola);
+console.log(letra);
+
+// Constantes primitivas
+const PI = 3.14;
+const LETRAA = 'A';
+const ESPACIO = ' ';
+
+console.log(ESPACIO);
