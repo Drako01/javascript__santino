27d4d1ed84d0b74f9d19f4b6222009b5f1c8db9b
Alejandro Di Stefano <addistefano@live.com.ar>
2024-08-13 20:03:24 -0300

    Clase 03

---
+++ assets/js/index.js
@@ -0,0 +1,120 @@
+// Ciclos e Iteraciones
+// Ciclos por Conteo
+// Estructura for
+
+// Ciclos Condicionales
+// Estructura while
+// Estructura do While
+
+// let numero;
+// numero = 0;
+// console.log(numero);
+// numero = numero + 1;
+// console.log(numero);
+// numero = numero + 1;
+// console.log(numero);
+// numero = numero + 1;
+// console.log(numero);
+// numero = numero + 1;
+// console.log(numero);
+// numero = numero + 1;
+// console.log(numero);
+// console.log("Sin en for");
+
+// i++ es igual a decir i = i + 1
+// for (let i = 1; i <= 40; i++) {
+//     console.log(i);
+// }
+// console.log("Con en for");
+// Estructura del ciclo for
+// for (desde; hasta; autoincrementacion) {
+//     Lo que quieras que se repita.
+// }
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+// }
+
+
+
+// Ciclos por condiciones
+// let repetir = true;
+// while(repetir){
+//     console.log("Hasta el infinito y mas alla....")
+// }
+
+
+// let ingreseNumero = parseInt(prompt("Ingrese un numero"));
+// let valorIngresado = isNaN(ingreseNumero); // o true o false
+// if (valorIngresado) {
+//     console.log("Usted ha ingresado un String");
+//     while (valorIngresado) {
+//         ingreseNumero = parseInt(prompt("Ingrese un numero"));
+//         valorIngresado = isNaN(ingreseNumero);
+//     }
+// }
+// console.log("Tabla de Multiplicar del " + ingreseNumero);
+// for (let i = 1; i <= 10; i++) {
+//     let resultado = ingreseNumero * i;
+//     console.log(ingreseNumero + " X " + i + " = " + resultado);
+// }
+
+
+// do While
+
+// let repetir = false;
+
+// do {
+//     console.log("Solo una Vez")
+// } while (repetir);
+
+// let numeroDoWhile = 0;
+// do {
+//     numeroDoWhile = prompt("ingrese un numero")
+//     console.log(numeroDoWhile)
+// } while (parseInt(numeroDoWhile));
+
+// Sentencias Break y Continue
+
+// for(let i = 1; i < 10; i++){
+//     if(i == 5) {
+//         break;
+//     }
+//     console.log("Esta es la Iteracion Nro: " + i + " Usando Break");    
+// }
+
+// for(let i = 1; i < 10; i++){
+//     if(i == 5) {
+//         continue;
+//     }
+//     console.log("Esta es la Iteracion Nro: " + i + " Usando Continue");    
+// }
+
+// Estructura Switch
+
+let numeroDia = parseInt(prompt("Ingrese el dia de la semana (1-7)"))
+switch (numeroDia){
+    case 1:
+        console.log("Hoy es Lunes");
+        break;
+    case 2:
+        console.log("Hoy es Martes");
+        break;
+    case 3:
+        console.log("Hoy es Miercoles");
+        break;
+    case 4:
+        console.log("Hoy es Jueves");
+        break;
+    case 5:
+        console.log("Hoy es Viernes");
+        break;
+    case 6:
+        console.log("Hoy es Sabado");
+        break;
+    case 7:
+        console.log("Hoy es Domingo");
+        break;
+    default:
+        console.log("Ingrese el dia de la semana (1-7)");        
+        break;
+}
